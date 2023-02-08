d7b86db8cec462409c9fff28c2549a70651b48d5
rajdeep2462 <rajdeep2462@gmail.com>
2023-02-08 10:47:12 +0530

    Added login controller

---
+++ GitHubAPI/Controllers/LogInController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GitHubAPI.Controllers
+{
+    public class LogInController : ApiController
+    {
+        // GET: api/LogIn
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/LogIn/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/LogIn
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/LogIn/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/LogIn/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
